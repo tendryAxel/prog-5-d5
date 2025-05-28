8bfe9e18e9e654f198ceed32d00a4b0900095859
tendryAxel <hei.tendry@gmail.com>
2025-05-28 08:48:57 +0300

    feat: data storage in json

---
+++ src/repository/jsonDataBaseStorage.ts
@@ -0,0 +1,33 @@
+import { existsSync, readFileSync, writeFileSync } from 'fs'
+
+export class Database<TData> {
+    public data: TData
+
+    constructor(private readonly dbPath: string, initial: TData) {
+        this.data = this.load(initial)
+    }
+
+    public update = (data: Partial<TData>) =>
+        (this.data = { ...this.data, ...data })
+
+    public commit = () => this.persist(this.data)
+
+    private persist = (data: TData) =>
+        writeFileSync(this.dbPath, JSON.stringify(data))
+
+    private read = (): TData =>
+        JSON.parse(readFileSync(this.dbPath).toString()) as TData
+
+    private load = (initial: TData): TData => {
+        if (!existsSync(this.dbPath)) {
+            this.persist(initial)
+        }
+
+        const current = this.read()
+
+        return {
+            ...initial,
+            ...current,
+        }
+    }
+}
