5d2a012048d02d9f927501e6dc53fd96ebeeeb56
tendryAxel <hei.tendry@gmail.com>
2025-05-28 08:42:37 +0300

    chore: create type

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# La specification
+# The specification
 https://github.com/MiiJack/prog-5-d5/blob/main/README.md
---
+++ src/repository/model/Coffee.ts
@@ -0,0 +1,9 @@
+type CoffeeTemplate = {
+    id: string;
+    name: string;
+}
+
+type CoffeeStored = {
+    coffee: CoffeeTemplate;
+    quantity: number;
+}
